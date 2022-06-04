c232649b6b95e67ae6b87328b36f3f1a04b5c126
Naveen <naveen.chaurasia861@gmail.com>
2022-06-04 12:13:23 +0530

    first ccommit

---
+++ src/EntryPage.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+const { Component } = React
+//const [errorMessages, setErrorMessages] = useState({});
+  //const [isSubmitted, setIsSubmitted] = useState(false);
+
+const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+       // setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+     // setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  
+class EntryPage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentView: "logIn"
+    }
+  }
+
+  changeView = (view) => {
+    this.setState({
+      currentView: view
+    })
+  }
+
+  currentView = () => {
+    switch(this.state.currentView) {
+      case "signUp":
+        return (
+          <form>
+            <h2>Sign Up!</h2>
+            <fieldset>
+              <legend>Create Account</legend>
+              <ul>
+                <li>
+                  <label for="username">Username:</label>
+                  <input type="text" id="username" required/>
+                </li>
+                <li>
+                  <label for="email">Email:</label>
+                  <input type="email" id="email" required/>
+                </li>
+                <li>
+                  <label for="password">Password:</label>
+                  <input type="password" id="password" required/>
+                </li>
+              </ul>
+            </fieldset>
+            <button>Submit</button>
+            <button type="button" onClick={ () => this.changeView("logIn")}>Have an Account?</button>
+          </form>
+        )
+        break
+      case "logIn":
+        return (
+          <form onSubmit={handleSubmit}>
+            <h2>Welcome!</h2>
+            <fieldset>
+              <legend>Log In</legend>
+              <ul>
+                <li>
+                  <label for="username">Username:</label>
+                  <input type="text" id="username" required/>
+                  {/* {renderErrorMessage("uname")} */}
+                </li>
+                <li>
+                  <label for="password">Password:</label>
+                  <input type="password" id="password" required/>
+                  {/* {renderErrorMessage("uname")} */}
+                </li>
+                <li>
+                  <i/>
+                  {/* <a onClick={ () => this.changeView("PWReset")} href="#">Forgot Password?</a> */}
+                </li>
+              </ul>
+            </fieldset>
+            <div>
+            <input type="submit" />
+            <button>Login</button>
+            </div>
+            {/* <button type="button" onClick={ () => this.changeView("signUp")}>Create an Account</button> */}
+          </form>
+        )
+        break
+      case "PWReset":
+        return (
+          <form>
+          <h2>Reset Password</h2>
+          <fieldset>
+            <legend>Password Reset</legend>
+            <ul>
+              <li>
+                <em>A reset link will be sent to your inbox!</em>
+              </li>
+              <li>
+                <label for="email">Email:</label>
+                <input type="email" id="email" required/>
+              </li>
+            </ul>
+          </fieldset>
+          <button>Send Reset Link</button>
+          <button type="button" onClick={ () => this.changeView("logIn")}>Go Back</button>
+        </form>
+        )
+      default:
+        break
+    }
+  }
+
+
+  render() {
+    return (
+      <section id="entry-page">
+        {this.currentView()}
+      </section>
+    )
+  }
+}
+
+//ReactDOM.render(<EntryPage/>, document.getElementById("app"))
+export default EntryPage;
